cd9f5f3ffcac389087f81c66255f3b247df26126
Phạm Minh Tiến <phminhtien136@gmail.com>
2025-01-15 14:32:38 +0700

    ui: chat page

--- chatapp/src/pages/Chat/Chat.tsx
+++ chatapp/src/pages/Chat/Chat.tsx
@@ -1,9 +1,16 @@
 import './Chat.scss'
+import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
+import ChatBox from '../../components/ChatBox/ChatBox'
+import RightSidebar from '../../components/RightSidebar/RightSidebar'
 
 const Chat = () => {
   return (
-    <div>
-      <h1>Chat</h1>
+    <div className="chat">
+      <div className="chat-container">
+        <LeftSidebar />
+        <ChatBox />
+        <RightSidebar />
+      </div>
     </div>
   )
 }
