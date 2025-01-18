15e607f12495399a209feba0cb159fd8579d97c2
Phạm Minh Tiến <phminhtien136@gmail.com>
2025-01-19 00:11:32 +0700

    Implement navigation logic for chat and profile pages.

--- chatapp/src/App.tsx
+++ chatapp/src/App.tsx
@@ -6,21 +6,29 @@ import { useEffect } from 'react'
 import { auth } from './config/firebase'
 import { onAuthStateChanged } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
+import { useAppDispatch } from './hooks/redux'
+import { loadUserData } from './store/appSlice'
 
 const App = () => {
 
   const navigate = useNavigate()
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     onAuthStateChanged(auth, async (user) => {
       if (user) {
         navigate('/chat')
+        const userData = await dispatch(loadUserData(user.uid))
+        console.log(userData)
+        if (loadUserData.fulfilled.match(userData)) {
+          navigate(userData.payload.redirect);
+        }
       } else {
         navigate('/')
       }
     })
   }, [])
-  
+
   return (
     <>
       <Routes>
@@ -29,7 +37,7 @@ const App = () => {
         <Route path="/profile-update" element={<ProfileUpdate />} />
       </Routes>
     </>
- )
+  )
 }
 
 export default App
--- chatapp/src/store/appSlice.ts
+++ chatapp/src/store/appSlice.ts
@@ -2,6 +2,7 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 import { doc, getDoc } from 'firebase/firestore'
 import { db } from '../config/firebase'
+import { useNavigate } from 'react-router-dom'
 
 interface AppState {
     // Thêm các state cần thiết ở đây
@@ -17,9 +18,10 @@ const initialState: AppState = {
 
 export const loadUserData = createAsyncThunk(
     'app/loadUserData',
-    async (uid: string) => {
+    async (uid: string, { dispatch }) => {
         const userDoc = await getDoc(doc(db, "users", uid));
-        return userDoc.data();
+        const userData = userDoc.data();
+        return { userData, redirect: userData?.avatar && userData?.name ? '/chat' : '/profile-update' };
     }
 );
 
@@ -36,7 +38,7 @@ export const appSlice = createSlice({
     },
     extraReducers: (builder) => {
         builder.addCase(loadUserData.fulfilled, (state, action) => {
-            state.userData = action.payload;
+            state.userData = action.payload.userData;
         });
     }
 });
